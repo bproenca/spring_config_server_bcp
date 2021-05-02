21d29d7d5fd7b61b2bc3f3822e822f8d3db32b38
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-05-02 16:09:51 -0300

    inject map props

--- service-a/src/main/java/bcp/Configuration.java
+++ service-a/src/main/java/bcp/Configuration.java
@@ -1,14 +1,18 @@
 package bcp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("service-a")
 public class Configuration {
-
+    
     private String role;
     private String pass;
+    private Map<String, DbTenant> dbtenants = new HashMap<>();
 
     protected Configuration() { }
 
@@ -28,6 +32,32 @@ public class Configuration {
         this.pass = pass;
     }
 
-    
+    public Map<String, DbTenant> getDbtenants() {
+        return dbtenants;
+    }
+
+    public void setDbtenants(Map<String, DbTenant> dbtenants) {
+        this.dbtenants = dbtenants;
+    }
+
+    public static class DbTenant {
+        private String host;
+        private Integer port;
+
+        public String getHost() {
+            return host;
+        }
+        public void setHost(String host) {
+            this.host = host;
+        }
+        public Integer getPort() {
+            return port;
+        }
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        
+    }
 
 }
--- service-a/src/main/java/bcp/MyController.java
+++ service-a/src/main/java/bcp/MyController.java
@@ -1,10 +1,14 @@
 package bcp;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import bcp.Configuration.DbTenant;
 
 @RestController
 public class MyController {
@@ -16,4 +20,15 @@ public class MyController {
     public String whoami(@PathVariable("username") String username) {
         return String.format("Hello! You're %s (%s) and you'll become a(n) %s...\n", username, configuration.getPass(), configuration.getRole());
     }
+
+	@GetMapping("/conn/{tenant}")
+	public DbTenant conn(@PathVariable(value = "tenant") String tenant) {
+		return configuration.getDbtenants().get(tenant);
+	}
+
+    @GetMapping("/all")
+	public Map<String, DbTenant> all() {
+		return configuration.getDbtenants();
+	}
+
 }
